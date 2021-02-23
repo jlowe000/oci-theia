927679c2c60ae5cd49ed4d21269434829d955e40
Jason Lowe <jlowe000@gmail.com>
2021-02-23 14:23:20 +1000

    changed the script to default to python but allows a docker tag to be used as input

--- bin/theia-ide-run.sh
+++ bin/theia-ide-run.sh
@@ -1,1 +1,5 @@
-docker run --rm -d -v "/home/oracle/project:/home/project:cached" --hostname theia-ide --name theia-ide --network theia_network theiaide/theia-python:1.10.0
+THEIA_IMAGE=theiaide/theia-python:1.10.0
+if [ "$1" != "" ]; then
+    THEIA_IMAGE=$1
+fi
+docker run --rm -d -v "/home/oracle/project:/home/project:cached" --hostname theia-ide --name theia-ide --network theia_network $THEIA_IMAGE
